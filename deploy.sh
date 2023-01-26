625578dc1c02dd2c8691e0f8849fbaaa241b94eb
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 10:46:58 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name wbh-caf-cpf-env --image nexus.sigame.com.br/fission-webhook-caf-cpf:0.1.0-1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name wbh-caf-cpf-fn --env wbh-caf-cpf-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission env create --spec --name wbh-caf-cpf-env --image nexus.sigame.com.br/fission-webhook-caf-cpf:0.1.0-1 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name wbh-caf-cpf-fn --env wbh-caf-cpf-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
 fission route create --spec --name wbh-caf-cpf-rt --method POST --url /webhook/caf/cpf_validation --function wbh-caf-cpf-fn
